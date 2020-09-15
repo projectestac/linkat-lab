6088ef30a284c6d2dc8a39fac20020458b0bd551
Joan de Gracia <joan.degracia@gmail.com>
2020-09-15 18:30:19 +0200

    Scripts definitius

--- linkat-stat/src/usr/share/linkat/linkat-stat/linkat-stat.sh
+++ linkat-stat/src/usr/share/linkat/linkat-stat/linkat-stat.sh
@@ -10,75 +10,60 @@
 # Dependències: nmap, dbus, dmidecode, virt-what
 #
 OD=$(which od)
-COUNTER=0
-LIMIT_COUNTER=4
-FLAG=0
-
-if [ ! -f /etc/lk-stat-counter ]; then
-   echo $COUNTER > /etc/lk-stat-counter
-   FLAG=1
+echo "0" > /etc/lk-stat-counter
+WAIT_TIME="5m"
+ID_MACHINE="$(/usr/sbin/dmidecode -s system-uuid | sha1sum | cut -d " " -f 1)"
+VERSION="$(lsb_release -r| sed -e 's/\t//g' |cut -d ":" -f 2)"
+ARCH="$(uname -m)"
+if [ $ARCH == "x86_64" ]; then
+   ARCH="x86-64"
 else
-   COUNTER=$(cat /etc/lk-stat-counter)
-   let 'COUNTER += 1'
-   echo $COUNTER > /etc/lk-stat-counter
+   ARCH="i386"
 fi
-let 'COUNTER %= LIMIT_COUNTER'
-if [ $COUNTER -eq 0 ] || [ $FLAG -eq 1 ]; then
-   echo "0" > /etc/lk-stat-counter
-   WAIT_TIME="5m"
-   ID_MACHINE="$(/usr/sbin/dmidecode -s system-uuid | sha1sum | cut -d " " -f 1)"
-   VERSION="$(lsb_release -r| sed -e 's/\t//g' |cut -d ":" -f 2)"
-   ARCH="$(uname -m)"
-   if [ $ARCH == "x86_64" ]; then
-      ARCH="x86-64"
-   else
-      ARCH="i386"
-   fi
-   VIRTUALIZATION="$(virt-what |head -n1)"
-   if [ -z "$VIRTUALIZATION" ]; then
-      VIRTUALIZATION="physical"
-   fi
-   URL="download-linkat.xtec.cat"
-   CADENA="STAT-LK"
-   UBUNTU_DESKTOP="$(LANG=C apt list *ubuntu-desktop 2>/dev/null |grep -i "instal\|upgra" |cut -d "/" -f 1)"
-   case $UBUNTU_DESKTOP in
-   ubuntu-desktop)
-      LINKAT_DESKTOP="gnome"
-   ;;
-   lubuntu-desktop)
-      LINKAT_DESKTOP="lxde"
-   ;;
-   esac
-   #
-   # Checking Network Connectivity
-   #
+VIRTUALIZATION="$(virt-what |head -n1)"
+if [ -z "$VIRTUALIZATION" ]; then
+   VIRTUALIZATION="physical"
+fi
+URL="download-linkat.xtec.cat"
+CADENA="STAT-LK"
+UBUNTU_DESKTOP="$(LANG=C apt list *ubuntu-desktop 2>/dev/null |grep -i "instal\|upgra" |cut -d "/" -f 1)"
+case $UBUNTU_DESKTOP in
+ubuntu-desktop)
+   LINKAT_DESKTOP="gnome"
+;;
+lubuntu-desktop)
+   LINKAT_DESKTOP="lxde"
+;;
+esac
+#
+# Checking Network Connectivity
+#
 
-   ESPERA=$($OD -A n -N 2 -t u2 /dev/urandom )
-   let 'ESPERA %= 32' # mòdul -> residu: 0-31
-   let 'ESPERA += 58'
-   # sleep 1s -> 1 segon / sleep 1m -> 1 minut
-   #sleep $ESPERA
-   FLAG=0
-   TEST_IPS="educaciodigital.cat ubuntu.com wikipedia.org"
-   TARGET=($(echo $TEST_IPS))
-   N_TARGET=${#TARGET[@]}
-   let 'N_TARGET -=1'
-   TEST_PORT=443
-   CURRENT_TEST_IP=0
-   while [ $FLAG -eq 0 ];
-   do
-      TESTIP=${TARGET[$CURRENT_TEST_IP]}
-      if [ -z "$(nmap  -p $TEST_PORT $TESTIP  2>/dev/null |grep open)" ]; then
-         if [ $CURRENT_TEST_IP -lt $N_TARGET ]; then   
-      	   let 'CURRENT_TEST_IP +=1'
-      	else
-      	   CURRENT_TEST_IP=0
-            sleep $WAIT_TIME
-         fi
-      else
-         FLAG=1
+ESPERA=$($OD -A n -N 2 -t u2 /dev/urandom )
+let 'ESPERA %= 32' # mòdul -> residu: 0-31
+let 'ESPERA += 58'
+# sleep 1s -> 1 segon / sleep 1m -> 1 minut
+sleep $ESPERA
+FLAG=0
+TEST_IPS="educaciodigital.cat ubuntu.com wikipedia.org"
+TARGET=($(echo $TEST_IPS))
+N_TARGET=${#TARGET[@]}
+let 'N_TARGET -=1'
+TEST_PORT=443
+CURRENT_TEST_IP=0
+while [ $FLAG -eq 0 ];
+do
+   TESTIP=${TARGET[$CURRENT_TEST_IP]}
+   if [ -z "$(nmap  -p $TEST_PORT $TESTIP  2>/dev/null |grep open)" ]; then
+      if [ $CURRENT_TEST_IP -lt $N_TARGET ]; then   
+   	   let 'CURRENT_TEST_IP +=1'
+   	else
+   	   CURRENT_TEST_IP=0
+         sleep $WAIT_TIME
       fi
-   done
-   curl -s $URL/${CADENA}_${ID_MACHINE}_${VERSION}_${LINKAT_DESKTOP}_${ARCH}_${VIRTUALIZATION} -o /dev/null
-fi
-   exit 0
+   else
+      FLAG=1
+   fi
+done
+curl -s $URL/${CADENA}_${ID_MACHINE}_${VERSION}_${LINKAT_DESKTOP}_${ARCH}_${VIRTUALIZATION} -o /dev/null
+exit 0
